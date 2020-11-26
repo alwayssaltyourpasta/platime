8bf11c30d8a89571e47c10a9ad20a42b78f36ecf
alwayssaltyourpasta <aleksandraakaaczmarskaa@gmail.com>
2020-11-26 17:37:33 +0100

    optimization of statistics

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
 from kivymd.app import MDApp
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 from design import builder_string
 from kivy.core.window import Window
-from kivymd.uix.picker import MDDatePicker
-from kivymd.uix.list import TwoLineListItem
 from kivy.utils import get_color_from_hex
-from functools import partial
-from kivymd.uix.list import IRightBodyTouch, OneLineAvatarIconListItem
-from kivymd.uix.selectioncontrol import MDCheckbox
-from kivy.properties import StringProperty
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.expansionpanel import MDExpansionPanel, MDExpansionPanelTwoLine
 import sqlite3
-from kivy.uix.checkbox import CheckBox
-from kivymd.uix.dialog import MDDialog
-from kivymd.uix.button import MDFlatButton
-from kivy.properties import NumericProperty
-from kivymd.uix.label import MDLabel
-from kivymd.uix.expansionpanel import MDExpansionPanel, MDExpansionPanelTwoLine
 from kivy.clock import Clock
+import datetime
+from datetime import datetime, timedelta
+from kivymd.uix.button import MDRoundFlatButton
+from kivymd.uix.list import ThreeLineListItem
 
 
 #screens python files
@@ -51,6 +41,118 @@ class Start(Screen):
 
     def on_enter(self, *args):
         Clock.schedule_once(self.skip, 0)
+
+#analysis function
+def summary(self, number_of_days, main_list):
+    dt = datetime.today()
+    days = dt.date()-timedelta(days=number_of_days)
+    print(days)
+
+    mycursor = sqliteConnection.cursor()
+    mycursor.execute(f"SELECT count(id_task) "
+                        f"FROM work_plan "
+                        f"WHERE done_date > ? AND done_date <= date('now') ", (days,))
+    row = mycursor.fetchone()
+    done_tasks = row[0]
+    print(done_tasks)
+
+    main_list.add_widget(
+        MDRoundFlatButton(
+            text=str(done_tasks) + "\n\nD O N E\nT A S K S",
+            text_color=get_color_from_hex('#e5e5e5'),
+            pos_hint={"x": .05, "y": .0},
+            size_hint=(0.27, 0.35),
+            font_size=15
+            )
+        )
+    mycursor.execute(f"SELECT count(id_task) "
+                         f"FROM work_plan "
+                         f"WHERE scheduled_date > ? AND scheduled_date <= date('now') ", (days,))
+    row = mycursor.fetchone()
+    all_scheduled_tasks = row[0]
+    if all_scheduled_tasks == None:
+        precent = 'PRO8LEM'
+    else:
+        precent = int(done_tasks/all_scheduled_tasks*100)
+    print(precent)
+    main_list.add_widget(
+        MDRoundFlatButton(
+            text=str(precent) + "%\n\nD O N E\nT A S K S",
+            text_color=get_color_from_hex('#e5e5e5'),
+            md_bg_color=get_color_from_hex('#333333'),
+            pos_hint={"x": .365, "y": 0},
+            size_hint=(0.27, 0.35),
+            font_size=15
+            )
+        )
+    mycursor.execute("SELECT sum(execution_time) "
+                        "FROM work_plan "
+                        "WHERE done_date <= date('now') AND done_date > ? ", (days,))
+    rows = mycursor.fetchone()
+    time = rows[0]
+    main_list.add_widget(
+        MDRoundFlatButton(
+            text=str(time) + "\nminutes\n\nW O R K\nT I M E",
+            text_color=get_color_from_hex('#e5e5e5'),
+            pos_hint={"x": .68, "y": 0},
+            size_hint=(0.27, 0.35),
+            font_size=15
+            )
+        )
+
+def analysis(self, number_of_days, scroll_list):
+    dt = datetime.today()
+    days = dt.date() - timedelta(days=number_of_days)
+    print(days)
+    mycursor = sqliteConnection.cursor()
+    mycursor.execute(
+        f"select a.task_name, a.scheduled_time, avg(b.execution_time), count(b.id_task), count(b.done_date) "
+        f"from task_type a "
+        f"join work_plan b "
+        f"on a.id_type = b.id_type "
+        f"where b.scheduled_date <= date('now') AND scheduled_date > ?"
+        f"group by b.id_type ", (days,))
+    rows = mycursor.fetchall()
+
+    task_name = []
+    task_time = []
+    avg_time = []
+    number_of_task = []
+    done_tasks = []
+
+    for i in range(len(rows)):
+        task_name.append(rows[i][0])
+        task_time.append(rows[i][1])
+        avg_time.append(rows[i][2])
+        number_of_task.append(rows[i][3])
+        done_tasks.append(rows[i][4])
+    print(number_of_task)
+    print(done_tasks)
+    for i in range(len(task_name)):
+
+        if avg_time[i] == None:
+            time_precent = "None"
+
+        else:
+            time_precent = str(int(avg_time[i] / task_time[i] * 100)) + "%"
+
+        if done_tasks[i] == None:
+            task_precent = "None"
+        else:
+            time_precent = str(int(done_tasks[i] / number_of_task[i] * 100)) + "%"
+
+        if avg_time[i] == None:
+            avg_time[i] = str(avg_time[i])
+        else:
+            avg_time[i] = int(avg_time[i])
+
+        scroll_list.add_widget(
+            ThreeLineListItem(
+                text=str(task_name[i]),
+                secondary_text=str(done_tasks[i]) + "/" + str(number_of_task[i]) + "/" + time_precent,
+                tertiary_text=str(task_time[i]) + "/" + str(avg_time[i]) + "/" + time_precent
+            )
+        )
 
 
 #main class - app
